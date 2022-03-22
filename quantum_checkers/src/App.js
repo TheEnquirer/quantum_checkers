c46c42ba54d6a0a0f2b494fa9c162bcc706661e4
annliz <annlicalifornia@gmail.com>
2022-03-22 09:40:52 -0700

    yesjf

--- quantum_checkers/src/App.js
+++ quantum_checkers/src/App.js
@@ -1,9 +1,14 @@
 import './App.css';
 
 function App() {
+  const poggers = () => {
+    
+    console.log('poggers');
+  }
   return (
       <div className="border-2 border-red-500">
 	uwu
+  {poggers()}
     </div>
   );
 }
